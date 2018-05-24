949ff24d228376d30795dbd1796d026ac3b1d2b8
meakommo <meako98@gmail.com>
2018-05-24 12:42:35 +1200

    added app.jsx prototype

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+class App extends React.Component {
+  render () {
+    return (
+      <Router>
+      </Router>
+    )
+  }
+}
+
+export default App
