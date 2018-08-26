aa6d7a022764549c19fa7d1929e35eff21945ab4
meakommo <meako98@gmail.com>
2018-08-26 14:23:10 +1200

    redux getting state

--- client/apiClient.js
+++ client/apiClient.js
@@ -5,12 +5,3 @@ export function getAllPosts () {
   return consume('get', '/api/v1/home')
     .then(res => res.body)
 }
-
-// import request from 'superagent'
-
-// export function getAllPosts () {
-//   return request.get('/api/v1/home')
-//     .then(res => {
-//       return res.body
-//     })
-// }
